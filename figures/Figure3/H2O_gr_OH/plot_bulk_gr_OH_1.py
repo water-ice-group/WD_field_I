315b62f39441359cbc8524478d96359a52f72004
Yair Litman <yairlitman@gmail.com>
2025-09-18 18:47:59 +0200

    upload figure3 gr and VDOS

--- figures/Figure3/H2O_gr_OH/plot_bulk_gr_OH_1.py
+++ figures/Figure3/H2O_gr_OH/plot_bulk_gr_OH_1.py
@@ -25,22 +25,25 @@ plt.rc('lines', markersize=8, linewidth=2)
 plt.rc('legend', frameon=True,framealpha=1,)
 plt.rcParams['figure.figsize'] = [4,4]
 plt.rc('text', usetex=False)
+#plt.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
 matplotlib.rcParams['mathtext.default'] = 'regular'
 
+#rc("font", **{"family": "sans-serif", "sans-serif": ["Arial"]})
 ticksize=18
 labelsize=20
 legendsize=15
 
-
-
 all_data=[]
 
-all_data.append(np.loadtxt('data_orientation/0000.dat'))
-all_data.append(np.loadtxt('data_orientation/0020.dat'))
-all_data.append(np.loadtxt('data_orientation/0040.dat'))
-all_data.append(np.loadtxt('data_orientation/0060.dat'))
-field=np.array([0.00, 0.002,0.004,0.006] )
-field*=51.422061454950224 #au2eV/au2A
+all_data.append(np.loadtxt('data/000.dat'))
+all_data.append(np.loadtxt('data/002.dat'))
+all_data.append(np.loadtxt('data/004.dat'))
+all_data.append(np.loadtxt('data/006.dat'))
+all_data.append(np.loadtxt('data/008.dat'))
+
+field=np.array([0.00, 0.002,0.004,0.006,0.008] )
+field_au2evA=51.422061454950224
+field*=field_au2evA
 
 def get_color_gradient(n,init=0.0,fin=1.0):                                                                                                                                                             
     """ Generate n distinct colors from a colormap. """                                                                                                                                                 
@@ -55,25 +58,30 @@ ticksize=18
 labelsize=20                                                                                                                                                                                            
 legendsize=15                                                                                                                                                                                           
 ndata=len(field)                                                                                                                                                                                  
-ndata_col=5
-colors = get_color_gradient(ndata_col,0.8,0.0)                                                                                                                                                              
+colors = get_color_gradient(ndata,0.8,0.0)                                                                                                                                                              
+colors[-1]='black'
 fig, ax = plt.subplots()#figsize=(6,6))                                                                                                                                                                   
+plt.grid(True, linestyle="--", linewidth=0.7, color="gray")
+
+linestyle=[]
+for i in range(ndata):
+ linestyle.extend('-')
+linestyle[-1]='--'
 
 for i in range(ndata):
     data=all_data[i]                                                                                                                                                                                       
-    ax.plot(np.arange(data.size)*2,data, label=r' {:3.2f} '.format(field[i])+r'V/$\mathrm{\AA}$',color=colors[i])
-
-ax.set_xlabel(r'time (fs)', fontsize=labelsize)
-ax.set_ylabel(r"Norm. orientation", fontsize=labelsize)
-filename='figure4c'
-ax.set_xlim([0,1000])
-ax.set_ylim([-0.1,1.1])                                                                                                                                                                                 
-
+    print(data.shape)
+    ax.plot(data[:,0],data[:,1], label=r' {:3.2f} '.format(field[i])+r'V/$\mathrm{\AA}$',linestyle=linestyle[i],color=colors[i])   
+ax.set_xlabel(r'r ($\mathrm{\AA}$)', fontsize=labelsize)                                                                                                                                                
+ax.set_ylabel(r"gr$_\mathrm{OH}$ ", fontsize=labelsize)                                                                                                                                             
+filename='bulk_grOH_1'
+ax.set_xlim([1.3,6.2])
+ax.set_xlim([1.3,4])
+ax.set_ylim([0,2])                                                                                                                                                                                 
 plt.xticks(fontsize=ticksize)                                                                                                                                                                           
 plt.yticks(fontsize=ticksize)                                                                                                                                                                           
 plt.locator_params(axis='x', nbins=5)                                                                                                                                                                   
 plt.locator_params(axis='y', nbins=5)                                                                                                                                                                   
-                                                                                                                                                                                                        
-plt.legend(loc='upper center',ncol=2, fancybox=True, shadow=True,fontsize=legendsize, bbox_to_anchor=(0.5,1.30))                                                                                        
+plt.legend(loc='upper center',ncol=3, fancybox=True, shadow=True,fontsize=legendsize, bbox_to_anchor=(0.5,1.30))                                                                                        
 plt.savefig('{}.pdf'.format(filename),bbox_inches='tight')                                                                                                                                              
 print('{}.pdf'.format(filename))                                                                                                                                                                        
